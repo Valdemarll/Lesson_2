6b1eb752a1ba6546b34ac9502ced6d1837b55e84
Valdemar <vladimirsmirnov69706@gmail.com>
2021-08-24 16:05:19 +0300

    Добавил объекты классов Book и Movie

--- src/L1_hw/Main.java
+++ src/L1_hw/Main.java
@@ -13,6 +13,7 @@ public class Main {
         System.out.println(human1);
         System.out.println(human2);
 
+
         // class Calculator
         Calculator calc = new Calculator();
 
@@ -20,6 +21,7 @@ public class Main {
         System.out.println(calc.sub(13, 2));
         System.out.println(calc.multiply(5, 7));
         System.out.println(calc.div(32, 8));
+
 
         // class Matrix
         Matrix matrix1 = new Matrix();
@@ -33,6 +35,35 @@ public class Main {
 
         System.out.println("Сложение Matrix1 и Matrix2:\n" + matrix1.add(matrix2));
         System.out.println("Произведение Matrix1 и Matrix2:\n" + matrix1.multiply(matrix2));
+
+
+        // class Book
+        Book book = new Book("Мастер и Маргарита", 480, 1966, "Булгаков");
+        System.out.println(book.getTitle());
+        System.out.println(book.getAuthor());
+        System.out.println(book.getYearRelease());
+        System.out.println(book.getPageCount());
+
+        book.setTitle("Гарри Поттер и Философский камень");
+        book.setAuthor("Джоан Роулинг");
+        book.setYearRelease(1997);
+        book.setPageCount(432);
+
+        System.out.println(book.toString());
+
+
+        // class Movie
+        Movie movie = new Movie("Free Guy", 267, "Зак Пенн");
+        System.out.println(movie.getTitle());
+        System.out.println(movie.getDirector());
+        System.out.println(movie.getPageCount());
+
+        movie.setTitle("Groundhog Day");
+        movie.setDirector("Харольд Ремис");
+        movie.setPageCount(361);
+
+        System.out.println(movie.toString());
+
     }
 
 }
