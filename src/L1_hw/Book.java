1d4caccc8ed3419f9da8d09d02011ae9fce1caad
Valdemar <vladimirsmirnov69706@gmail.com>
2021-08-24 15:44:21 +0300

    Добавил конструктор и get/set методы

--- src/L1_hw/Book.java
+++ src/L1_hw/Book.java
@@ -5,4 +5,42 @@ public class Book {
     int pageCount;
     int yearRelease;
     String author;
+
+    Book(String title, int pageCount, int yearRelease, String author){
+        this.title = title;
+        this.pageCount = pageCount;
+        this.yearRelease = yearRelease;
+        this.author = author;
+    }
+    void setTitle(String title){
+        this.title = title;
+    }
+    void setPageCount(int pageCount){
+        this.pageCount = pageCount;
+    }
+    void setYearRelease(int yearRelease){
+        this.yearRelease = yearRelease;
+    }
+    void setAuthor(String author){
+        this.author = author;
+    }
+
+    String getTitle(){
+        return title;
+    }
+    int getPageCount(){
+        return pageCount;
+    }
+    int getYearRelease(){
+        return yearRelease;
+    }
+    String getAuthor(){
+        return author;
+    }
+
+    @Override
+    public String toString(){
+        return "Книга '" + title + "' содержит в себе " + pageCount + " страниц(ы/у). " +
+                author + " выпустил(а) ее в " + yearRelease + " году.";
+    }
 }
--- src/L1_hw/Movie.java
+++ src/L1_hw/Movie.java
@@ -4,4 +4,34 @@ public class Movie {
     String title;
     int pageCount;
     String director;
+
+    Movie(String title, int pageCount, String director){
+        this.title = title;
+        this.pageCount = pageCount;
+        this.director = director;
+    }
+    void setTitle(String title){
+        this.title = title;
+    }
+    void setPageCount(int pageCount){
+        this.pageCount = pageCount;
+    }
+    void setDirector(String director){
+        this.director = director;
+    }
+
+    String getTitle(){
+        return title;
+    }
+    int getPageCount(){
+        return pageCount;
+    }
+    String getDirector(){
+        return director;
+    }
+
+    @Override
+    public String toString(){
+        return "Сценарий '" + title + "' содержит в себе " + pageCount + " страниц(ы/у). Режиссер сценария: " + director;
+    }
 }
