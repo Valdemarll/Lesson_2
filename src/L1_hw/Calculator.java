c52c2e0821e7e231849d7e89d6f529cf23e22326
Valdemar <vladimirsmirnov69706@gmail.com>
2021-08-24 12:13:19 +0300

    Создан класс Calculator с методами сложения, вычитания, умножения и деления

---
+++ src/L1_hw/Calculator.java
@@ -0,0 +1,16 @@
+package L1_hw;
+
+public class Calculator {
+    int add(int x1, int x2){
+        return x1 + x2;
+    }
+    int sub(int x1, int x2){
+        return x1 - x2;
+    }
+    int multiply(int x1, int x2){
+        return x1 * x2;
+    }
+    int div(int x1, int x2){
+        return x1 / x2;
+    }
+}
